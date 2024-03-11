4de73668fa2e732c84504746edcfcf7635d7b32c
Andres Rendo <andres.rendo@correo.unimet.edu.ve>
2024-03-11 16:24:16 -0400

    Creacion del sistemas y matriz

---
+++ main.py
@@ -0,0 +1,119 @@
+# import numpy as np
+# import random
+from matrix import Matriz
+
+# Crear una matriz de 3x3 con valores aleatorios entre 0 y 9
+
+def trespor3(matriz, altura):
+    for i in range  (altura):
+        X = int(input(f"Introduzca el valor de X en la ecuacion {i+1}: "))
+        if X > 0:
+            Xec = f"{X}"
+        elif X == 0:
+            Xec = f"+{X}"
+        else:
+            Xec = f"{X}"
+        print(f"{Xec}X + Y + Z = 0 \n")
+        Y = int(input(f"Introduzca el valor de Y en la ecuacion {i+1}: "))
+        if Y > 0:
+            Yec = f"+{Y}"
+        elif Y == 0:
+            Yec = f"+{Y}"
+        else:
+            Yec = f"{Y}"
+        print(f"{Xec}X {Yec}Y + Z = 0 \n")
+        Z = int(input(f"Introduzca el valor de Z en la ecuacion {i+1}: "))
+        if Z > 0:
+            Zec = f"+{Z}"
+        elif Z == 0:
+            Zec = f"+{Z}"
+        else:
+            Zec = f"{Z}"
+        print(f"{Xec}X {Yec}Y {Zec}Z = 0 \n")
+        matriz.modificar_valor(i, 0, X)
+        matriz.modificar_valor(i, 1, Y)
+        matriz.modificar_valor(i, 2, Z)
+        igualdad = input(f"Introduzca el valor de igualdad en la ecuacion {i+1}: ")
+        print(f"{Xec}X {Yec}Y {Zec}Z = {igualdad}\n")
+        matriz.modificar_igualdad(i, igualdad)
+    print("El sistema de ecuaciones es: \n")
+    print(f"|{matriz.obtener_valor(0, 0)}X {matriz.obtener_valor(0, 1)}Y {matriz.obtener_valor(0, 2)}Z = {matriz.igualdad1}")
+    print(f"|{matriz.obtener_valor(1, 0)}X {matriz.obtener_valor(1, 1)}Y {matriz.obtener_valor(1, 2)}Z = {matriz.igualdad2}")
+    print(f"|{matriz.obtener_valor(2, 0)}X {matriz.obtener_valor(2, 1)}Y {matriz.obtener_valor(2, 2)}Z = {matriz.igualdad3}")
+    return matriz
+
+def cuatropor4(matriz, altura):
+    for i in range  (altura):
+        X = input(f"Introduzca el valor de X en la ecuacion {i+1}: ")
+        if X > 0:
+            Xec = f"{X}"
+        elif X == 0:
+            Xec = f"+{X}"
+        else:
+            Xec = f"{X}"
+        print(f"{Xec}X + Y + Z + W = 0 \n")
+        Y = input(f"Introduzca el valor de Y en la ecuacion {i+1}: ")
+        if Y > 0:
+            Yec = f"+{Y}"
+        elif Y == 0:
+            Yec = f"+{Y}"
+        else:
+            Yec = f"{Y}"
+        Z = input(f"Introduzca el valor de Z en la ecuacion {i+1}: ")
+        if Z > 0:
+            Zec = f"+{Z}"
+        elif Z == 0:
+            Zec = f"+{Z}"
+        else:
+            Zec = f"{Z}"
+        W = input(f"Introduzca el valor de W en la ecuacion {i+1}: ")
+        if W > 0:
+            Wec = f"+{W}"
+        elif W == 0:
+            Wec = f"+{W}"
+        else:
+            Wec = f"{W}"
+        print(f"{Xec}X {Yec}Y {Zec}Z {Wec}W = 0 \n")
+        matriz.modificar_valor(i, 0, X)
+        matriz.modificar_valor(i, 1, Y)
+        matriz.modificar_valor(i, 2, Z)
+        matriz.modificar_valor(i, 3, W)
+        igualdad = input(f"Introduzca el valor de igualdad en la ecuacion {i+1}: ")
+        print(f"{Xec}X {Yec}Y {Zec}Z {Wec}W = {igualdad}\n")
+        matriz.modificar_igualdad(i, igualdad)
+    print("El sistema de ecuaciones es: \n")
+    print(f"|{matriz.obtener_valor(0, 0)}X {matriz.obtener_valor(0, 1)}Y {matriz.obtener_valor(0, 2)}Z {matriz.obtener_valor(0, 3)}W = {matriz.igualdad1}")
+    print(f"|{matriz.obtener_valor(1, 0)}X {matriz.obtener_valor(1, 1)}Y {matriz.obtener_valor(1, 2)}Z {matriz.obtener_valor(1, 3)}W = {matriz.igualdad2}")
+    print(f"|{matriz.obtener_valor(2, 0)}X {matriz.obtener_valor(2, 1)}Y {matriz.obtener_valor(2, 2)}Z {matriz.obtener_valor(2, 3)}W = {matriz.igualdad3}")
+    print(f"|{matriz.obtener_valor(3, 0)}X {matriz.obtener_valor(3, 1)}Y {matriz.obtener_valor(3, 2)}Z {matriz.obtener_valor(3, 3)}W = {matriz.igualdad4}")
+    return matriz
+
+def main():
+    altura = input("Cuantas ecuaciones desea tener 3 o 4?: \n")
+    while altura != "3" and altura != "4":
+        print("Por favor introduzca un valor valido")
+        altura = input("Cuantas ecuaciones desea tener 3 o 4?: \n")
+    altura = int  (altura)
+    ancho = int(altura)
+    print("El sistema de ecuaciones es: \n")
+
+    if ancho == 3:
+        print("|X + Y + Z = 0")
+        print("|X + Y + Z = 0")
+        print("|X + Y + Z = 0")
+    elif ancho == 4:
+        print("|X + Y + Z + W = 0")
+        print("|X + Y + Z + W = 0")
+        print("|X + Y + Z + W = 0")
+        print("|X + Y + Z + W = 0")
+
+    matriz = Matriz (altura, ancho)  # Crear una matriz de nxm con valores iniciales 0
+
+    if ancho == 3:
+        matriz = trespor3(matriz, altura)
+    elif ancho == 4:
+        matriz = cuatropor4(matriz, altura)
+    matriz.mostrar_matriz()
+
+main()
+
---
+++ matrix.py
@@ -0,0 +1,41 @@
+# Create a class for a matrix
+class Matriz:
+    def __init__(self, filas, columnas, valor_inicial=0, igualdad1 = 0, igualdad2= 0, igualdad3 = 0, igualdad4 = 0):
+        self.filas = filas
+        self.columnas = columnas
+        self.matriz = [[valor_inicial for _ in range(columnas)] for _ in range(filas)]
+
+    def mostrar_matriz(self):
+        for fila in self.matriz:
+            print(fila)
+
+    def obtener_valor(self, fila, columna):
+        if fila < 0 or fila >= self.filas or columna < 0 or columna >= self.columnas:
+            print("Índices fuera de rango.")
+            return None
+        return self.matriz[fila][columna]
+
+    def modificar_valor(self, fila, columna, nuevo_valor):
+        if fila < 0 or fila >= self.filas or columna < 0 or columna >= self.columnas:
+            print("Índices fuera de rango.")
+            return
+        self.matriz[fila][columna] = nuevo_valor
+    
+    def modificar_igualdad(self, numFila, igualdad):
+        if numFila == 0:
+            self.igualdad1 = igualdad
+        elif numFila == 1: 
+            self.igualdad2 = igualdad
+        elif numFila == 2:
+            self.igualdad3 = igualdad
+        elif numFila == 3:
+            self.igualdad4 = igualdad
+            
+
+
+    def mostrar_igualdad(self):
+        print(f"El valor de igualdad es: {self.igualdad1}")
+        print(f"El valor de igualdad2 es: {self.igualdad2}")
+        print(f"El valor de igualdad3 es: {self.igualdad3}")
+        print(f"El valor de igualdad4 es: {self.igualdad4}")
+
