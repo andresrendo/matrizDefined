09081930c4547585477d58edb0edec8e2a483436
VirginiaSTR <71676092+VirginiaSTR@users.noreply.github.com>
2024-03-26 22:22:50 -0400

    Cambios en la Matrix

--- matrix.py
+++ matrix.py
@@ -1,41 +1,106 @@
-# Create a class for a matrix
+import numpy as np
+
+def solo_num(eje,i):
+        while True:
+            try:
+                num = int(input(f"Introduzca el valor de {eje} en la ecuacion {i+1}: "))
+                break
+            except ValueError:
+                print("Por favor, introduzca un número entero.")
+        return num
 class Matriz:
-    def __init__(self, filas, columnas, valor_inicial=0, igualdad1 = 0, igualdad2= 0, igualdad3 = 0, igualdad4 = 0):
+    def __init__(self, filas, columnas, valor_inicial=0):
         self.filas = filas
         self.columnas = columnas
-        self.matriz = [[valor_inicial for _ in range(columnas)] for _ in range(filas)]
+        self.matriz = np.full((filas, columnas), valor_inicial)
+        self.igualdades = np.zeros(filas, dtype=int)
+
 
     def mostrar_matriz(self):
-        for fila in self.matriz:
-            print(fila)
+        print(self.matriz)
+
 
     def obtener_valor(self, fila, columna):
-        if fila < 0 or fila >= self.filas or columna < 0 or columna >= self.columnas:
-            print("\nÍndices fuera de rango.")
-            return None
         return self.matriz[fila][columna]
 
+
     def modificar_valor(self, fila, columna, nuevo_valor):
-        if fila < 0 or fila >= self.filas or columna < 0 or columna >= self.columnas:
-            print("\nÍndices fuera de rango.")
-            return
         self.matriz[fila][columna] = nuevo_valor
-    
+
+
     def modificar_igualdad(self, numFila, igualdad):
-        if numFila == 0:
-            self.igualdad1 = igualdad
-        elif numFila == 1: 
-            self.igualdad2 = igualdad
-        elif numFila == 2:
-            self.igualdad3 = igualdad
-        elif numFila == 3:
-            self.igualdad4 = igualdad
-            
+        self.igualdades[numFila] = igualdad
 
 
     def mostrar_igualdad(self):
-        print(f"\nEl valor de igualdad es: {self.igualdad1}")
-        print(f"\nEl valor de igualdad2 es: {self.igualdad2}")
-        print(f"\nEl valor de igualdad3 es: {self.igualdad3}")
-        print(f"\nEl valor de igualdad4 es: {self.igualdad4}")
+        print("Igualdades:", self.igualdades)
 
+
+    def trespor3(matriz, altura):
+        for i in range(altura):
+            X = solo_num("X",i)
+            Y = solo_num("Y",i)
+            Z = solo_num("Z",i)
+            igualdad = solo_num("igualdad",i)
+            # Formatea variables con signos
+            Xec = "+" if X > 0 else ""
+            Yec = "+" if Y > 0 else ""
+            Zec = "+" if Z > 0 else ""
+            # Almacena ecuaciones y valor de igualdad
+            ecuacion = f"{Xec}{X}X {Yec}{Y}Y {Zec}{Z}Z = {igualdad}"
+            matriz.modificar_valor(i, 0, X)
+            matriz.modificar_valor(i, 1, Y)
+            matriz.modificar_valor(i, 2, Z)
+            matriz.modificar_igualdad(i, igualdad)
+
+        print("\nEl sistema de ecuaciones es:")
+        for i in range(altura):
+            print(f"\t |{matriz.obtener_valor(i, 0)}X\t {matriz.obtener_valor(i, 1)}Y\t {matriz.obtener_valor(i, 2)}Z\t =\t {matriz.igualdades[i]}|\n")
+        return matriz
+
+
+    def cuatropor4(matriz, altura):
+        for i in range(altura):
+            X = solo_num("X",i)
+            Y = solo_num("Y",i)
+            Z = solo_num("Z",i)
+            W = solo_num("W",i)
+            igualdad = solo_num("igualdad",i)
+
+            Xec = "+" if X > 0 else ""
+            Yec = "+" if Y > 0 else ""
+            Zec = "+" if Z > 0 else ""
+            Wec = "+" if W > 0 else ""
+
+            ecuacion = f"{Xec}{X}X {Yec}{Y}Y {Zec}{Z}Z {Wec}{W}W = {igualdad}"
+            matriz.modificar_valor(i, 0, X)
+            matriz.modificar_valor(i, 1, Y)
+            matriz.modificar_valor(i, 2, Z)
+            matriz.modificar_valor(i, 3, W)
+            matriz.modificar_igualdad(i, igualdad)
+
+        print("\nEl sistema de ecuaciones es:")
+        for i in range(altura):
+            print(f"\t |{matriz.obtener_valor(i, 0)}X\t {matriz.obtener_valor(i, 1)}Y\t {matriz.obtener_valor(i, 2)}Z\t {matriz.obtener_valor(i, 3)}W\t =\t {matriz.igualdades[i]}|\n")
+        return matriz
+
+
+    def verificar_matriz_bien_definida(self):
+        for i in range(self.filas):
+            suma_fila = sum(abs(self.matriz[i, j]) for j in range(self.columnas) if i != j)
+            if abs(self.matriz[i, i]) <= suma_fila:
+                return False
+        return True
+
+    def solucion_jacobi(self):
+        x = np.zeros_like(self.igualdades)
+        D = np.diag(self.matriz)
+        R = self.matriz - np.diagflat(D)
+        for cuenta_iteracion in range(100):
+
+            x_new = (self.igualdades - np.dot(R,x))/D
+            x = x_new
+
+
+        print("\nSolution:")
+        print(f'\t{x}')   
